bb429e0c10681befdb83ea000eec6afd2c29ba59
Guido Princ <guido.princ@wolox.com.ar>
2018-06-11 17:10:10 -0300

    Updated analytics navigation tracking

--- generators/app/templates/src/redux/middlewares/analyticsMiddleware.js
+++ generators/app/templates/src/redux/middlewares/analyticsMiddleware.js
@@ -1,22 +1,28 @@
 import Config from 'react-native-config';
 import { NavigationActions } from 'react-navigation';
 import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
-import GoogleAnalytics, { trackEvent } from '@redux-beacon/react-native-google-analytics';
+import GoogleAnalytics, {
+  trackScreenView
+} from '@redux-beacon/react-native-google-analytics';
 import { createMiddleware } from 'redux-beacon';
 
 import { getCurrentRouteName } from '../../utils/navUtils';
 
 export const checkAnalyticsTrackingID = () => {
   if (!Config.ANALYTICS_TRACKING_ID) {
-    console.warn('Google Analytics Tracking ID has not been properly initialized');
+    console.warn(
+      'Google Analytics Tracking ID has not been properly initialized'
+    );
   }
 };
 
-const target = GoogleAnalytics(Config.ANALYTICS_TRACKING_ID, GoogleAnalyticsTracker);
+const target = GoogleAnalytics(
+  Config.ANALYTICS_TRACKING_ID,
+  GoogleAnalyticsTracker
+);
 
-const pageView = trackEvent((action, prevState) => ({
-  hitType: 'pageview',
-  page: getCurrentRouteName(prevState.nav)
+const pageView = trackScreenView((action, prevState) => ({
+  screenName: getCurrentRouteName(prevState.nav)
 }));
 
 const eventsMap = {
